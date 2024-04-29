d079b3093c70394c53415068f07ce74510d6d850
Vedica Mrudul <vedicaam80@gmail.com>
2024-04-29 16:45:04 +0530

    updated footer and header

---
+++ src/components/header/LogoutBtn.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { logout } from '../../store/authSlice'
+import authService from '../../appwrite/auth'
+import { useDispatch } from 'react-redux'
+
+function LogoutBtn() {
+    const handleClick = () => {
+        authService.logout().then(() => {
+            dispatch(logout())
+        }).catch((err) => {
+            console.error(err)
+        })
+    }
+
+
+  return (
+    <>
+    <button className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full' onClick={handleClick}>LogOut</button>
+    </>
+  )
+}
+
+export default LogoutBtn
